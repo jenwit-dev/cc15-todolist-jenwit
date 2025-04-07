389afa6cbc7e1eb3d3d8942ef22ef5d27e7ac14d
jenwit-dev <johnny.js19@gmail.com>
2025-04-07 11:04:48 +0700

    refactor:searchbar

---
+++ src/Header.jsx
@@ -0,0 +1,21 @@
+import { FaHome, FaSearch } from "react-icons/fa";
+import "./Header.scss";
+import Search from "./Search";
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="header__logo">
+        <FaHome />
+      </div>
+      <div className="header__text">
+        <h1>TodoList</h1>
+      </div>
+      <div className="header__search">
+        <Search />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
